LineageOS/android_hardware_amlogic_kernel-modules_rtl8822cs

rtl8822cs: Fix compilation using kbuild
Change-Id: I951c1565cc6a1efc0df9d57abedbe198c94e41a2

rtl88x2CS/hal/phydm/phydm.mk
@@ -1,4 +1,4 @@
-EXTRA_CFLAGS += -I$(src)/hal/phydm
+EXTRA_CFLAGS += -I$(KERNEL_SRC)/$(M)/hal/phydm
 
 _PHYDM_FILES := hal/phydm/phydm_debug.o	\
 								hal/phydm/phydm_antdiv.o\

LineageOS/android_hardware_amlogic_kernel-modules_rtl8822bs

hal/phydm/phydm.mk
@@ -1,4 +1,4 @@
-EXTRA_CFLAGS += -I$(src)/hal/phydm
+EXTRA_CFLAGS += -I$(srctree)/$(src)/hal/phydm
 
 _PHYDM_FILES := hal/phydm/phydm_debug.o	\
 								hal/phydm/phydm_antdiv.o\